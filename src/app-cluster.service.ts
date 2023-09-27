eef8ad5154c7ee8763a2780778ae393a5f00d965
Saad <skhatimiti@gmail.com>
2023-09-28 00:58:22 +0200

    SIGINT added

--- src/app-cluster.service.ts
+++ src/app-cluster.service.ts
@@ -8,9 +8,21 @@ export class AppClusterService {
   static clusterize(callback: Function): void {
     if (cluster.isPrimary) {
       Logger.log(`Master server started on ${process.pid}`);
+      //ensure workers exit cleanly 
+      process.on('SIGINT', function () {
+        Logger.log('Cluster shutting down...');
+        for (var id in cluster.workers) {
+          cluster.workers[id].kill();
+        }
+        // exit the master process
+        process.exit(0);
+      });
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
       }
+      cluster.on('online', function (worker) {
+        Logger.log(`Worker ${worker.process.pid} is online`);
+      });
       cluster.on('exit', (worker) => {
         Logger.log(`Worker ${worker.process.pid} died. Restarting`);
         cluster.fork();
