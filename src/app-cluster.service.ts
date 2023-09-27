045fe2cdcad2a24667d21b21bc627bbb0f99469c
Saad <skhatimiti@gmail.com>
2023-09-28 00:43:09 +0200

    logger added cluster service

--- src/app-cluster.service.ts
+++ src/app-cluster.service.ts
@@ -1,22 +1,22 @@
 const cluster = require('cluster');
 import { cpus } from 'os';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 const numCPUs = cpus().length;
 @Injectable()
 export class AppClusterService {
   static clusterize(callback: Function): void {
     if (cluster.isPrimary) {
-      console.log(`Master server started on ${process.pid}`);
+      Logger.log(`Master server started on ${process.pid}`);
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
       }
       cluster.on('exit', (worker) => {
-        console.log(`Worker ${worker.process.pid} died. Restarting`);
+        Logger.log(`Worker ${worker.process.pid} died. Restarting`);
         cluster.fork();
       });
     } else {
-      console.log(`Cluster server started on ${process.pid}`);
+      Logger.log(`Cluster server started on ${process.pid}`);
       callback();
     }
   }
